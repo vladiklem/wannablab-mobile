845943fe6b6c4b958a24681d286f4c121a6ae873
vladiklem <lemvlad99@gmail.com>
2020-05-06 21:44:41 +0300

    Final setup.

---
+++ packages/app/ios/Pods/Target Support Files/Pods-wannablabMobile-wannablabMobileTests/Pods-wannablabMobile-wannablabMobileTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wannablabMobile_wannablabMobileTests : NSObject
+@end
+@implementation PodsDummy_Pods_wannablabMobile_wannablabMobileTests
+@end
