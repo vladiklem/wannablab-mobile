9a0dabb3331e8e7ab51ad54b6228eb10d473b893
valeriiamykhalova <valeriia.mykhalova@gmail.com>
2020-07-01 03:15:31 +0300

    [wb-7] Add new colors to app theme

--- packages/app/src/theme/colors/index.js
+++ packages/app/src/theme/colors/index.js
@@ -1,9 +1,13 @@
 export default {
   $black: '#000000',
   $text: '#323D43',
+  $title: '#424F57',
   $error: '#F5355C',
   $placeholder: '#8898AA',
   $white: '#ffffff',
   $lightPurple: '#CBBAED',
   $facebook: '#0880DE',
+  $primary: '#37A3F2',
+  $secondary: '#5E72E4',
+  $green: '#4ED69B',
 };
